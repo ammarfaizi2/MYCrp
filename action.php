23c5bd78a7007a028e001dafec41f97ea9fd4c6e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-08 17:13:11 +0700

    Update action.php

--- action.php
+++ action.php
@@ -1,3 +1,12 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config/config.php';
+define('data',__DIR__ . '/data');
+is_dir(data) or mkdir(data);
+use System\ActionHandler;
+
+foreach ($config as $val) {
+	foreach ($val as $data) {
+		$app = new ActionHandler($data['token']);
+	}
+}

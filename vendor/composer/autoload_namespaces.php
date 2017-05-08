fad28aea5416bc764464d8cf5c9f3b920bd672e0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-08 16:49:14 +0700

    composer dumpautoload

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'System' => array($baseDir . '/mycrp'),
     'Curl' => array($vendorDir . '/ammarfaizi2/ratools/src'),
 );

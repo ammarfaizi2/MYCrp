89bd367d752c0589409f614b3f13d104c8bb08a6
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-18 21:17:04 +0700

    Create get_token method

--- action.php
+++ action.php
@@ -1,12 +1,13 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/config/config.php';
 define('data',__DIR__ . '/data');
 define("fb_data", data."/fb_data");
 is_dir(data) or mkdir(data);
 is_dir(fb_data) or mkdir(fb_data);
 use System\ActionHandler;
+require __DIR__ . '/config/config.php';
 
+die;
 foreach ($config as $val) {
 	(new ActionHandler($val))->run();
 }
